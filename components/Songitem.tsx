fe9d819cf2deed550677af1b0e27aea6e98bba98
DNaunin <d.naunin@gmail.com>
2021-02-16 20:47:20 +0100

    add a few songitems

--- components/Songitem.tsx
+++ components/Songitem.tsx
@@ -1,15 +1,17 @@
+import styles from "../styles/Songitem.module.css";
+
 type Props = {
   artist: string;
   title: string;
-  image: src;
+  image: string;
 };
 
 export default function Songitem(props: Props) {
   return (
-    <div>
-      <img src="https://images.unsplash.com/photo-1598099947145-e85739e7ca28?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80"></img>
-      <h4>F Your Purse</h4>
-      <p>The Read</p>
+    <div className={styles.songitem}>
+      <img className={styles.song__image} src={props.image} />
+      <h4 className={styles.song__title}>{props.title}</h4>
+      <p className={styles.song__artist}>{props.artist}</p>
     </div>
   );
 }
