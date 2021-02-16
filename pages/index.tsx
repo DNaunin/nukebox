9f0357b848c66be1a5e180e6ce229fdd95d73382
DNaunin <d.naunin@gmail.com>
2021-02-16 16:43:53 +0100

    add variables and delete components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,15 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Greeting from "../components/Greeting";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Nukebox</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+      <Greeting name="Dörte" />
     </div>
   );
 }
