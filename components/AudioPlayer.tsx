afb450c37e6c7005002b335cee37b28f2c80da91
DNaunin <d.naunin@gmail.com>
2021-02-25 11:10:32 +0100

    some refactoring

--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -1,10 +1,56 @@
+import { useEffect, useRef, useState } from "react";
 import styles from "../styles/Audioplayer.module.css";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 type Props = {
-  audio: string;
+  id: string;
+  src: string;
 };
 
-export default function AudioPlayer({ audio }: Props) {
+export default function AudioPlayer({ src, id }: Props) {
+  const audioRef = useRef(new Audio(src));
+  const intervalRef = useRef<NodeJS.Timeout>();
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [progress, setProgress] = useState(0);
+
+  const audioElement = audioRef.current;
+
+  // const [favorite, setFavorite] = useState(null);
+  const [favoriteSongs, setFavoriteSongs] = useLocalStorage<string[]>(
+    "favoriteSongs",
+    []
+  );
+  const favorite = favoriteSongs.includes(id);
+
+  // useEffect(() => {
+  //   if (typeof id != "string" || favorite === null) {
+
+  const handleFavoriteClick = () => {
+    if (favorite) {
+      const newFavoriteSongs = favoriteSongs.filter(
+        (favoriteSong) => favoriteSong !== id
+      );
+      setFavoriteSongs(newFavoriteSongs);
+    } else {
+      setFavoriteSongs([...favoriteSongs, id]);
+    }
+  };
+
+  //   return;
+  // }
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioElement.play();
+      intervalRef.current = setInterval(() => {
+        setProgress(audioElement.currentTime);
+      }, 2000);
+    } else {
+      clearInterval(intervalRef.current);
+      audioElement.pause();
+    }
+  }, [isPlaying]);
+
   return (
     <figure className={styles.audiofigure}>
       <figcaption className={styles.playercaption}>
@@ -13,20 +59,31 @@ export default function AudioPlayer({ audio }: Props) {
           className={styles.download}
           alt="fake download button"
         />
-        <img
-          src="/Heart.svg"
-          className={styles.heart}
-          alt="fake favourite button"
-        />
+        <button className={styles.favbutton} onClick={handleFavoriteClick}>
+          {favorite ? <img src="/Heart.svg" /> : "🖤"}
+        </button>
         <img
           src="/Volume.svg"
           className={styles.volume}
           alt="fake volume button"
         />
       </figcaption>
-      <audio controls src={audio}>
-        <code>audio</code> element not supported.
-      </audio>
+      <div className={styles.audioPlayer}>
+        <input
+          className={styles.duration}
+          type="range"
+          min="0"
+          max={audioElement.duration}
+          value={progress}
+        />
+
+        <button className={styles.btn} onClick={() => setIsPlaying(!isPlaying)}>
+          <img
+            className="btn__image"
+            src={isPlaying ? "/pauseButton.svg" : "/playButton.svg"}
+          />
+        </button>
+      </div>
     </figure>
   );
 }
